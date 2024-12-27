d8565080801b8301f0b0c419065f1cb0d7714d29
Mathias Verhasselt <mathiasverhasselt@Mathiass-MacBook-Pro.local>
2024-12-27 11:56:29 -0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/three_js/sw.js', { scope: '/three_js/' })})}
